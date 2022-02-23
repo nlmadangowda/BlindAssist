efb6a7c35e80298d093d01bcaf4f3d2a450b0a92
Madan Kumar N L <71085343+nlmadangowda@users.noreply.github.com>
2022-02-23 20:28:30 +0530

    Blind Assist Initial commit

---
+++ BlindStick.py
@@ -0,0 +1,165 @@
+# https://cloud.google.com/vision/docs/setup
+
+from google.cloud import vision
+import io
+
+
+def detect_faces(image):
+    """Detects faces in an image."""
+    response = client.face_detection(image=image)
+    faces = response.face_annotations
+
+    # Names of likelihood from google.cloud.vision.enums
+    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                       'LIKELY', 'VERY_LIKELY')
+    print('Faces:')
+
+    for face in faces:
+        print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
+        print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
+        print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in face.bounding_poly.vertices])
+
+        print('face bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+
+def detect_labels(image):
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    print('Labels:')
+
+    for label in labels:
+        print(label.description)
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+
+def detect_crop_hints(image):
+    """Detects crop hints in an image."""
+    crop_hints_params = vision.CropHintsParams(aspect_ratios=[1.77])
+    image_context = vision.ImageContext(
+        crop_hints_params=crop_hints_params)
+
+    response = client.crop_hints(image=image, image_context=image_context)
+    hints = response.crop_hints_annotation.crop_hints
+
+    for n, hint in enumerate(hints):
+        print('\nCrop Hint: {}'.format(n))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in hint.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+
+def detect_landmarks(image):
+    """Detects landmarks in the file."""
+    response = client.landmark_detection(image=image)
+    landmarks = response.landmark_annotations
+    print('Landmarks:')
+
+    for landmark in landmarks:
+        print(landmark.description)
+        for location in landmark.locations:
+            lat_lng = location.lat_lng
+            print('Latitude {}'.format(lat_lng.latitude))
+            print('Longitude {}'.format(lat_lng.longitude))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+
+def detect_logos(image):
+    """Detects logos in the file."""
+    response = client.logo_detection(image=image)
+    logos = response.logo_annotations
+    print('Logos:')
+
+    for logo in logos:
+        print(logo.description)
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+
+def localize_objects(image):
+    """Localize objects in the local image.
+
+    Args:
+    path: The path to the local file.
+    """
+    objects = client.object_localization(
+        image=image).localized_object_annotations
+
+    print('Number of objects found: {}'.format(len(objects)))
+    for object_ in objects:
+        print('\n{} (confidence: {})'.format(object_.name, object_.score))
+        print('Normalized bounding polygon vertices: ')
+        for vertex in object_.bounding_poly.normalized_vertices:
+            print(' - ({}, {})'.format(vertex.x, vertex.y))
+
+
+def pic_to_text(image):
+    """Detects text in an image file
+
+    ARGS
+    infile: path to image file
+
+    RETURNS
+    String of text detected in image
+    """
+
+    # Instantiates a client
+
+    # Opens the input image file
+    # For dense text, use document_text_detection
+    # For less dense text, use text_detection
+    response = client.document_text_detection(image=image)
+    text = response.full_text_annotation.text
+    print("Detected text: {}".format(text))
+
+    return text
+
+
+
+
+path = 'res/img1.jpg'
+
+client = vision.ImageAnnotatorClient()
+
+with io.open(path, 'rb') as image_file:
+    content = image_file.read()
+
+image = vision.Image(content=content)
+
+detect_faces(image)
+detect_labels(image)
+detect_crop_hints(image)
+detect_landmarks(image)
+detect_logos(image)
+localize_objects(image)
+pic_to_text(image)
---
+++ app.py
@@ -0,0 +1,28 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+file_name = os.path.abspath('random.jpg')
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+image = vision.Image(content=content)
+
+# Performs label detection on the image file
+response = client.label_detection(image=image)
+labels = response.label_annotations
+
+print('Labels:')
+for label in labels:
+    print(label.description)
+
+
+#https://www.analyticsvidhya.com/blog/2021/11/build-face-recognition-attendance-system-using-python/
